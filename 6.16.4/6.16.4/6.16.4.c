e7606e22f5f5a4c79425d4a0468bf1ef7af2fb22
TXC-Trista <947631684@qq.com>
2022-08-14 00:06:54 +0800

    《C Primer Plus》（第6版）中文版 6.16编程练习

---
+++ 6.16.4/6.16.4/6.16.4.c
@@ -0,0 +1,24 @@
+/*6.16.4. 使用嵌套循环，按下面的格式打印字母：
+
+如果你的系统不适用以数字顺序编码的代码，请参照练习3的方案解决。*/
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main(void)
+{
+	int x;   //行
+	int y;   //下标
+	int k;   
+	int sum = 0;
+	
+	for (x = 0; x < 6; x++)
+	{
+		k = sum;
+		for (y = k; y <= k + x; y++)
+		{
+			printf("%c", 'A' + y);
+			sum++;
+		}
+		printf("\n");
+	}
+	return 0;
+}
