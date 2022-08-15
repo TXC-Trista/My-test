e1da55ebc4bd83668500df61376155feed199995
TXC-Trista <947631684@qq.com>
2022-08-15 23:57:04 +0800

    第六章编程习题第5题

--- 6.16.4/6.16.4/6.16.4.c
+++ 6.16.4/6.16.4/6.16.4.c
@@ -1,5 +1,10 @@
 /*6.16.4. 使用嵌套循环，按下面的格式打印字母：
-
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
 如果你的系统不适用以数字顺序编码的代码，请参照练习3的方案解决。*/
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
@@ -8,7 +13,7 @@ int main(void)
 	int x;   //行
 	int y;   //下标
 	int k;   
-	int sum = 0;
+	int sum = 0;  //打印次数
 	
 	for (x = 0; x < 6; x++)
 	{
