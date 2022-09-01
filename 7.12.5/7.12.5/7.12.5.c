176755ceb9278ac97e5a985c654311c1c15e5d0c
TXC-Trista <947631684@qq.com>
2022-09-01 22:40:24 +0800

    《C Primer Plus》第七章 编程习题

---
+++ 7.12.5/7.12.5/7.12.5.c
@@ -0,0 +1,27 @@
+/*7.12.5. 用switch重写练习4。*/
+/*7.12.4. 使用if else 语句编写一个整数读入，读到#停止。用感叹号替换句号，用两个
+感叹号替换原来的感叹号，最后报告进了多少次替换。*/
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main(void)
+{
+	char ch;
+	char a = 0;    //转换次数
+	while ((ch = getchar()) != '#')
+	{
+		switch (ch)
+		{
+		case '.':
+			ch = '!';
+			++a;
+		case '!':
+			ch = '!!';
+			++a;
+			break;
+		default:
+			break;
+		}
+	}
+	printf("一共转换了：%d 次\n", a);
+	return 0;
+}
